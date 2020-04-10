5827e717d584eb49cca814cb218bfaae03b30c09
unknown <escowichmartin@gmail.com>
2020-04-10 14:34:00 -0300

    resuelvo paso 02

--- src/died/guia06/Alumno.java
+++ src/died/guia06/Alumno.java
@@ -3,7 +3,7 @@ package died.guia06;
 import java.util.List;
 
 
-public class Alumno {
+public class Alumno implements Comparable<Alumno> {
 
 	private String nombre;
 	private Integer nroLibreta;
@@ -11,17 +11,30 @@ public class Alumno {
 	private List<Curso> aprobados;
 
 	public int creditosObtenidos() {
-		return 1;
+		int credObt = 0;
+		for(Curso c: aprobados) {
+			credObt += c.getCreditos();
+		}
+		return credObt;
 	}
-
+	
 	public void aprobar(Curso c) {
-		//
+		cursando.remove(c);
+		aprobados.add(c);
 	}
 
 	public void inscripcionAceptada(Curso c) {
-		//
+		cursando.add(c);
 	}
 	
-	
-
+	public boolean equals(Alumno a) {
+		if(this.nroLibreta == a.nroLibreta) {
+			return true;
+		}
+		else { return false;}
+	}
+	@Override
+	public int compareTo(Alumno a) {
+		return this.nombre.compareTo(a.nombre);
+	}
 }
